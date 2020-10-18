80025357a137755932fe8f5e8771f18efcd756be
Andrej <glazba@gmail.com>
2020-10-18 22:25:12 +0200

    psuh

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for Axis input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Apply input to "Movement", actually Root rotation
+        //transform.Ro += horizontalInput;
+        //transform.rotation.y += verticalInput;
+
+
+    }
+}
